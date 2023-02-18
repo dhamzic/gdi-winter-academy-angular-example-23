fc0a9dcf6ad42e81fc6dc60c85d4ad756944c073
Domagoj Hamzić <domagoj.hamzic@gdi.net>
2023-02-18 12:33:00 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SocketService } from './shared/services/socket.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gdi-winter-academy-demo-frontend-homemade-amateur-verified';
+
+  constructor(private socketService: SocketService){}
+
+  ngOnInit(): void{
+    this.socketService.buildConnection();
+  }
 }
---
+++ src/app/movie-details/movie-details.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IMovie } from '../shared/models/IMovie';
+import { MoviesService } from '../shared/services/movies.service';
+import { Location } from '@angular/common';
+import { IDropdown } from '../shared/models/IDropdown';
+import { IActor } from '../shared/models/IActor';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.scss']
+})
+export class MovieDetailsComponent implements OnInit {
+
+  actors: IDropdown[] = [];
+
+  movie: IMovie | undefined;
+
+  constructor(private route: ActivatedRoute, private moviesService: MoviesService, private location: Location) { }
+
+  ngOnInit(): void {
+    this.refreshData();
+  }
+
+  refreshData() {
+    let movieId = this.route.snapshot.paramMap.get('id');
+    if (movieId != null) {
+      this.moviesService.getMovie(+movieId).subscribe((movie: IMovie) => {
+        this.movie = movie;
+      });
+
+      this.moviesService.getActorsDropdown().subscribe((actors: IDropdown[]) => {
+        this.actors = actors;
+      });
+    }
+  }
+
+  onSubmit() {
+    if (this.movie != undefined) {
+      this.moviesService.modifyMovie(this.movie).subscribe(() => {
+        alert(`${this.movie?.title} has been successfully modified.`);
+      });
+    }
+  }
+
+  refreshEventMethod(modifiedActor: IActor) {
+    this.actors.find(x => x.value == modifiedActor.id)!.label = modifiedActor.name;
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+}
